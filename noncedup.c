8da6426c40a9bf186f7bd5e9bb3fbc9b4146c7b7
Zwilla <michael.padilla@zwilla.de>
2016-09-07 23:08:05 +0200

    Version 4.9.2 BMMiner to CGminer- 7th September 2016

--- noncedup.c
+++ noncedup.c
@@ -1,5 +1,6 @@
 /*
  * Copyright 2014 Andrew Smith
+ * formatting 2016 by Miguel Padilla
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
@@ -27,6 +28,7 @@ struct dupdata {
 	uint64_t dups;
 };
 
+
 void dupalloc(struct cgpu_info *cgpu, int timelimit)
 {
 	struct dupdata *dup;
@@ -42,14 +44,18 @@ void dupalloc(struct cgpu_info *cgpu, int timelimit)
 	cgpu->dup_data = dup;
 }
 
+
 void dupcounters(struct cgpu_info *cgpu, uint64_t *checked, uint64_t *dups)
 {
 	struct dupdata *dup = (struct dupdata *)(cgpu->dup_data);
 
-	if (!dup) {
+	if (!dup)
+	{
 		*checked = 0;
 		*dups = 0;
-	} else {
+	}
+	else
+	{
 		*checked = dup->checked;
 		*dups = dup->dups;
 	}
@@ -63,37 +69,53 @@ bool isdupnonce(struct cgpu_info *cgpu, struct work *work, uint32_t nonce)
 	K_ITEM *item;
 
 	if (!dup)
+	{
 		return false;
+	}
 
 	cgtime(&now);
 	dup->checked++;
 	K_WLOCK(dup->nfree_list);
 	item = dup->nonce_list->tail;
-	while (unique && item) {
-		if (DATAN(item)->work_id == work->id && DATAN(item)->nonce == nonce) {
+
+	while (unique && item)
+	{
+		if (DATAN(item)->work_id == work->id && DATAN(item)->nonce == nonce)
+		{
 			unique = false;
 			applog(LOG_WARNING, "%s%d: Duplicate nonce %08x",
 					    cgpu->drv->name, cgpu->device_id, nonce);
-		} else
-			item = item->prev;
+		}
+        else
+        {
+            item = item->prev;
+        }
 	}
-	if (unique) {
+
+	if (unique)
+    {
 		item = k_unlink_head(dup->nfree_list);
 		DATAN(item)->work_id = work->id;
 		DATAN(item)->nonce = nonce;
 		memcpy(&(DATAN(item)->when), &now, sizeof(now));
 		k_add_head(dup->nonce_list, item);
 	}
+
 	item = dup->nonce_list->tail;
-	while (item && tdiff(&(DATAN(item)->when), &now) > dup->timelimit) {
+
+	while (item && tdiff(&(DATAN(item)->when), &now) > dup->timelimit)
+    {
 		item = k_unlink_tail(dup->nonce_list);
 		k_add_head(dup->nfree_list, item);
 		item = dup->nonce_list->tail;
 	}
+
 	K_WUNLOCK(dup->nfree_list);
 
 	if (!unique)
-		dup->dups++;
+    {
+        dup->dups++;
+    }
 
 	return !unique;
 }
